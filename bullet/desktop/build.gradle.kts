d8fe54aaed1009ce0503483b42cf127b5d89fefe
Natan <xpenatan@gmail.com>
2023-08-14 18:37:40 -0300

    add builder

--- bullet/desktop/build.gradle.kts
+++ bullet/desktop/build.gradle.kts
@@ -1,13 +1,9 @@
 val moduleName = "desktop"
 
-val jarFile = "$projectDir/../generator/build/c++/desktop/bullet-natives.jar"
+val windowsFile = "$projectDir/../generator/build/c++/desktop/bullet64.dll"
 
 tasks.jar {
-    from(zipTree(jarFile))
-}
-
-tasks.register<Jar>("platformAll") {
-    from(zipTree(jarFile))
+    from(windowsFile)
 }
 
 publishing {
